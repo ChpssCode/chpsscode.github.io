4f74d690f7983c4c361f0271907686dcccfc3fd0
Chpsterz <paaxyman@gmail.com>
2023-05-13 11:25:54 -0400

    Initial Commit

--- portfolio-website/src/components/aboutme.svelte
+++
@@ -1,4 +0,0 @@
-<div class="text-dark container text-center" style=" display: flex; font-size: 22px; padding-top:100px; padding-bottom: 100px; width: 690px; margin: auto;">
-    <img src="https://cdn.discordapp.com/avatars/726559229451829290/230dc7addc28a480db020340fa3f2bee.png?size=1024" alt="Chpsterz_pfp" width="200px" height="210px">
-    <div style="padding-left: 50px;">Hello World, Im Chpsterz! An avid 14 year old coder from the United States. I have been coding for around 2 years, and enjoy working on front-end development, game development, and algorithims! Here are some of my previous projects:</div>
-</div>
--- portfolio-website/src/components/header.svelte
+++
@@ -1,6 +0,0 @@
-<div style="background: url(https://images.pexels.com/photos/1169754/pexels-photo-1169754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)" class="jumbotron bg-cover text-white">
-    <div class="container text-center" style="padding-top:150px; padding-bottom: 150px;">
-        <h1 class="display-1 font-weight-bold">Chpsterz</h1>
-        <p class="font-italic mb-0 display-6">Full-stack developer.</p>
-    </div>
-</div>
